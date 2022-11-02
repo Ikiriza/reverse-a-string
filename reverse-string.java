f06d48ab1b536ed27603e1c55f8562a1c012d529
Gensi Collin <99279784+Ikiriza@users.noreply.github.com>
2022-11-02 11:29:30 +0300

    Update reverse-string.java

--- reverse-string.java
+++ reverse-string.java
@@ -5,7 +5,7 @@ public class Exercise37 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input a string: ");
         char[] letters = scanner.nextLine().toCharArray();
-        System.out.print("Reverse string: ");
+        System.out.print("Reverse string:!!");
         for (int i = letters.length - 1; i >= 0; i--) {
             System.out.print(letters[i]);
         }
