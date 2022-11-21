879cf7aab4980152acf1050476c12254b01ef08e
Gensi Collin <99279784+Ikiriza@users.noreply.github.com>
2022-11-21 10:45:12 +0300

    Update reverse-string.java

--- reverse-string.java
+++ reverse-string.java
@@ -1,6 +1,6 @@
 //This is how you reverse a string in java
 import java.util.Scanner;
-public class Exercise37 {
+public class Reverse {
      public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input a string: ");
@@ -9,6 +9,7 @@ public class Exercise37 {
         for (int i = letters.length - 1; i >= 0; i--) {
             System.out.print(letters[i]);
         }
+          
         System.out.print("\t");
     }
 }
